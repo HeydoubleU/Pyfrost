57781afa5b72feef3c6a334b4798d95dca768752
HeydoubleU <56705510+HeydoubleU@users.noreply.github.com>
2023-02-23 21:17:41 -0600

    Update Pyfrost.cpp

--- src/Pyfrost.cpp
+++ src/Pyfrost.cpp
@@ -1,13 +1,3 @@
-//-
-// =============================================================================
-// Copyright 2022 Autodesk, Inc. All rights reserved.
-//
-// Use of this software is subject to the terms of the Autodesk license
-// agreement provided at the time of installation or download, or which
-// otherwise accompanies this software in either electronic or hard copy form.
-// =============================================================================
-//+
-
 #include "python.h"
 #include "Pyfrost.h"
 #include <Windows.h>
